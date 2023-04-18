fb0beb43c63597773c08d06ae1510d32ba3b6250
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-04-18 17:52:16 +0530

    modified register button

--- front/src/Components/Heading.jsx
+++ front/src/Components/Heading.jsx
@@ -31,7 +31,7 @@ const Heading = () => {
         </div>
        
     </div>
-    <Link to="/register" className="relative mt-10 sm:w-full inline-flex items-center justify-center  mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+    <Link to="/register" className="relative mt-10  inline-flex items-center justify-center  mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                        <span className="relative px-5 py-2.5 transition-all ease-in duration-75  dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                        REGISTER HERE
                       </span>
